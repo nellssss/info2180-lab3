534d245a83e613670f257379f687c26386fad518
Shanel Whitfield <shanel.whitfield02@mymona.uwi.edu>
2025-10-14 18:44:58 -0500

    Board Layout

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+//Board Layout
+document.addEventListener('DOMContentLoaded', () => {
+    var brd = document.querySelectorAll('#board div');
+    brd.forEach(div => div.classList.add('square')); 
+});
+
+
+
