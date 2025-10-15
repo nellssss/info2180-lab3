672ca404c1c4859bfce7f9db639294573f417b3d
Shanel Whitfield <shanel.whitfield02@mymona.uwi.edu>
2025-10-15 14:11:40 -0500

    X/O added and tracked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,25 @@
-//Board Layout
 document.addEventListener('DOMContentLoaded', () => {
-    var brd = document.querySelectorAll('#board div');
+    //board layout
+    const brd = document.querySelectorAll('#board div');
     brd.forEach(div => div.classList.add('square')); 
+
+    //Adding X's and O's
+    let mv = 'X';
+    let plays = [];
+    
+    brd.forEach((div, player) => {
+        div.addEventListener('click', () => {
+            if (div.textContent === '') {
+                div.textContent = mv;
+                div.classList.add(mv === 'X' ? ('square','X'):('square','O'));
+                
+                plays[player] = mv; //record player moves
+                mv = mv === 'X' ? 'O':'X';
+
+                //console.log(plays);
+            }
+        })
+    })
+
 });
-
-
 
